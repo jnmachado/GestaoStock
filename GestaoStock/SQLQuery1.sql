b31d646475d3652313c722ae6710111e57bbc42b
jnmachado <58390464+jnmachado@users.noreply.github.com>
2020-03-25 21:48:22 +0000

    Remover Ficheiros Obsoletos

--- GestaoStock/SQLQuery1.sql
+++
@@ -1,3 +0,0 @@
-﻿insert into tbl_login values ('admin', 'admin', 1)
-
-select * from tbl_login
--- GestaoStock/SQLQuery2.sql
+++
@@ -1,8 +0,0 @@
-﻿create table tbl_mov(
-	datahora datetime,
-	username varchar(20),
-	produto varchar(50),
-	foreign key (username) references tbl_login(username),
-	foreign key (produto) references tbl_stock(ref),
-	primary key (username, datahora)
-)
